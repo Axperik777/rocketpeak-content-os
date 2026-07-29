f43a76bf5e8204fd009291475050d09cca757225
Axperik777 <goodmarketing.ge@gmail.com>
2026-07-29 12:35:49 +0400

    Grant media validator service access

--- supabase/migrations/20260729122500_fix_tiktok_channels_constraint.sql
+++ supabase/migrations/20260729122500_fix_tiktok_channels_constraint.sql
@@ -3,3 +3,6 @@ alter table public.posts drop constraint if exists posts_channels_check1;
 
 alter table public.posts add constraint posts_channels_check
   check (channels <@ array['Facebook', 'Instagram', 'TikTok']::text[]);
+
+grant select, update on table public.media_assets to service_role;
+grant select on table storage.objects to service_role;
